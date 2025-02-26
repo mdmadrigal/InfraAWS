f29cd82b54c28e0205138ea93d9cdf2580592001
Maurício Madrigal <mauriciodm@gmail.com>
2025-02-26 16:53:14 -0300

    variables

--- public.tf
+++ public.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "curso_subnet_pub_1a" {
   vpc_id                  = aws_vpc.curso_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -13,7 +13,7 @@ resource "aws_subnet" "curso_subnet_pub_1a" {
 
 resource "aws_subnet" "curso_subnet_pub_1b" {
   vpc_id                  = aws_vpc.curso_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "curso_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
   enable_dns_support   = true
+
   tags = {
     Name = "Curso EDN"
   }
