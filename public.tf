839dc5fb0b3dcd11b4cb618360f379efc131ec1e
Maurício Madrigal <mauriciodm@gmail.com>
2025-02-28 22:27:36 -0300

    tags

--- public.tf
+++ public.tf
@@ -1,23 +1,23 @@
 resource "aws_subnet" "curso_subnet_pub_1a" {
   vpc_id                  = aws_vpc.curso_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
-
   tags = {
-    Name = "Curso_subnet-1a"
+    Name                     = "Curso_subnet-1a"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
-
-resource "aws_subnet" "curso_subnet_pub_1b" {
+resource "aws_subnet" "curso_subnet_public_1b" {
   vpc_id                  = aws_vpc.curso_vpc.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Curso_subnet-1b"
+    Name                     = "Curso_subnet-1b"
+    "kubernetes.io/role/elb" = "1"
   }
 }
